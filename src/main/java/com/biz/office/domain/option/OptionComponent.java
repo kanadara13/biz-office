37e4e220d1d29b4c38f3034a00c7f5c879d71326
kanadara13 <shin.jaehyeok@leisureq.co.kr>
2018-03-08 00:57:58 +0900

    [office] OPTION COMPONENT FETCH.TYPE EAGER CHANGE

--- src/main/java/com/biz/office/domain/option/OptionComponent.java
+++ src/main/java/com/biz/office/domain/option/OptionComponent.java
@@ -3,6 +3,8 @@ package com.biz.office.domain.option;
 import com.biz.office.domain.base.BaseDomain;
 import com.biz.office.domain.item.Item;
 import com.google.common.collect.Lists;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.util.List;
@@ -13,8 +15,21 @@ public class OptionComponent extends BaseDomain {
     @Column
     private Long itemId;
 
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id", insertable = false, updatable = false)
+    private Item item;
+
     @Column
     private Long optionId;
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
 
     public Long getItemId() {
         return itemId;
--- src/test/java/com/biz/office/domain/option/OptionCriteriaTest.java
+++ src/test/java/com/biz/office/domain/option/OptionCriteriaTest.java
@@ -1,14 +1,10 @@
 package com.biz.office.domain.option;
 
-import com.biz.office.domain.base.CriteriaRequest;
 import com.biz.office.repository.option.OptionRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +12,6 @@ import javax.inject.Inject;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -35,9 +30,9 @@ public class OptionCriteriaTest {
     public void findAll() {
         List<Option> optionList = repository.findAll();
         optionList.forEach(e->{
-           /* e.getItems().forEach(item -> {
-                System.out.println(item.getName());
-            });*/
+             e.getComponents().forEach(component -> {
+                System.out.println(component.getItemId());
+            });
         });
     }
 
@@ -58,68 +53,5 @@ public class OptionCriteriaTest {
      /*   aOption.getItems().forEach(e->{
             System.out.println(e.getName());
         });*/
-    }
-
-    @Test
-    public void find() {
-        Pageable pageable = new Pageable() {
-            @Override
-            public int getPageNumber() {
-                return 0;
-            }
-
-            @Override
-            public int getPageSize() {
-                return 0;
-            }
-
-            @Override
-            public int getOffset() {
-                return 0;
-            }
-
-            @Override
-            public Sort getSort() {
-                return null;
-            }
-
-            @Override
-            public Pageable next() {
-                return null;
-            }
-
-            @Override
-            public Pageable previousOrFirst() {
-                return null;
-            }
-
-            @Override
-            public Pageable first() {
-                return null;
-            }
-
-            @Override
-            public boolean hasPrevious() {
-                return false;
-            }
-        };
-
-        Page<Option> options = criteria.find(new CriteriaRequest() {
-            @Override
-            public String getKeyword() {
-                return null;
-            }
-
-            @Override
-            public void setKeyword() {
-
-            }
-        }, pageable);
-        options.forEach(e->{
-            System.out.println(e.getName());
-           /* e.getItems().forEach(item -> {
-                System.out.println(item.getName());
-            });*/
-        });
     }
 }
